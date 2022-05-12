3471db87cd6f6b62d97c4bf619a09bc77aaf747b
hurshik7 <hurshik7@gmail.com>
2022-05-12 15:05:20 -0700

    add main-bg, grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">My-shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col xs>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%" alt="first"
+            />
+            <h4>Title</h4>
+            <p>description</p>
+          </Col>
+          <Col xs>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%" alt="second"
+            />
+            <h4>Title</h4>
+            <p>description</p>
+          </Col>
+          <Col xs>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%" alt="third"
+            />
+            <h4>Title</h4>
+            <p>description</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
