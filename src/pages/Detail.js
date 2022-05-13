f6f5581d2fccb0128dfa0fb884c464387e6adc8e
hurshik7 <hurshik7@gmail.com>
2022-05-13 11:15:41 -0700

    move Detail.js

---
+++ src/pages/Detail.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function Detail(props) {
+  let id = useParams();
+  if (parseInt(id.id) >= 3 || parseInt(id.id) < 0) {
+    return (
+      <div>Wrong item</div>
+    );
+  }
+  let selectedItem = props.shoes.filter((item) => {
+    return item.id === parseInt(id.id);
+  })
+  if (selectedItem.length !== 0) {
+    selectedItem = selectedItem[0];
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={`https://codingapple1.github.io/shop/shoes${selectedItem.id + 1}.jpg`}
+            width="100%"
+            alt={`shoes${selectedItem.id}`}
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{selectedItem.title}</h4>
+          <p>{selectedItem.content}</p>
+          <p>{selectedItem.price}</p>
+          <button className="btn btn-danger">Order</button>
+        </div>
+      </div>
+    </div>
+  );
+}
