7cff2c1db9c19f0a05071278bf9380d6193ed77a
hurshik7 <hurshik7@gmail.com>
2022-05-13 11:15:28 -0700

    edit Nav

--- src/MyNavBar.js
+++ src/MyNavBar.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function MyNavBar() {
+  let navigate = useNavigate();
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">My-shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
-            <Nav.Link href="#features"><Link to="/detail">Detail</Link></Nav.Link>
-            <Nav.Link href="#pricing"><Link to="/">Etc</Link></Nav.Link>
+            <Nav.Link href="#home" onClick={() => {
+              navigate('/');
+            }}>
+                Home
+            </Nav.Link>
+            <Nav.Link href="#features" onClick={() => {
+              navigate('/detail');
+            }}>
+                Detail
+            </Nav.Link>
+            <Nav.Link href="#pricing" onClick={() => {
+              navigate('/');
+            }}>
+                Etc
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
