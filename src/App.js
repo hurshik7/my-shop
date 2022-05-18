be3486318c4c65c07e44e3d951992656a33aded3
hurshik7 <hurshik7@gmail.com>
2022-05-18 15:12:37 -0700

    add Route

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import MyNavBar from "./MyNavBar";
 import data from "./data";
 import Product from "./Prouct";
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Detail from "./Detail";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Detail from "./pages/Detail";
 
 function App() {
   let [shoes, setShoes] = useState(data);
@@ -15,7 +15,7 @@ function App() {
       <MyNavBar />
 
       <Routes>
-        <Route path="/detail" element={<Detail/>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
         <Route
           path="/"
           element={
@@ -24,14 +24,39 @@ function App() {
               <Container>
                 <Row>
                   {shoes.map((element, i) => {
-                    return <Product img={i} data={shoes[i]} />;
+                    return <Product img={i} data={shoes[i]} key={i}/>;
                   })}
                 </Row>
               </Container>
             </>
           }
         />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<About />} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>Get a first order for a free Shoe lace</div>} />
+          <Route path="two" element={<div>Birthday coupon</div>} />
+        </Route>
+        <Route path="*" element={<div>404</div>} />
       </Routes>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>Company information</h4>
+    </div>
+  );
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>Event page</h4>
+      <Outlet></Outlet>
     </div>
   );
 }
