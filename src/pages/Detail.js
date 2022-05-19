8ca2b525e10fbe3486cfe3be121413109fc868c3
hurshik7 <hurshik7@gmail.com>
2022-05-19 16:56:16 -0700

    add useEffect

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 let BlackDiv = styled.div`
-  background : grey;
-  padding : 20px;
-`
+  background: grey;
+  padding: 20px;
+`;
+
 export default function Detail(props) {
+  useEffect(() => {
+    setTimeout(() => {setAlert(false)}, 2000);
+  }, []);
+
+  let [alert, setAlert] = useState(true);
   let id = useParams();
   if (parseInt(id.id) >= 3 || parseInt(id.id) < 0) {
-    return (
-      <div>Wrong item</div>
-    );
+    return <div>Wrong item</div>;
   }
   let selectedItem = props.shoes.filter((item) => {
     return item.id === parseInt(id.id);
-  })
+  });
   if (selectedItem.length !== 0) {
     selectedItem = selectedItem[0];
   }
 
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">
+          Get a discount by completing your order in 2 seconds!
+        </div>
+      ) : null}
       <BlackDiv></BlackDiv>
       <div className="row">
         <div className="col-md-6">
           <img
-            src={`https://codingapple1.github.io/shop/shoes${selectedItem.id + 1}.jpg`}
+            src={`https://codingapple1.github.io/shop/shoes${
+              selectedItem.id + 1
+            }.jpg`}
             width="100%"
             alt={`shoes${selectedItem.id}`}
           />
